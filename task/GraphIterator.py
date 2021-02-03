a518640011acf3348b6511c07f8bdfa1158b7dc8
YaroslavAggressive <55752143+YaroslavAggressive@users.noreply.github.com>
2021-02-03 15:17:37 +0500

    Added GraphIterator

---
+++ task/GraphIterator.py
@@ -0,0 +1,24 @@
+from Graph import Graph
+
+class GraphIterator:
+
+    def __init__(self, graph):
+        self.graph = graph
+        self.quan_of_sequences_for_sqientists = []
+        self.find_seq_quan_for_scientists()
+        self.quan_of_sequences_for_sqientists.sort(reverse=True)
+
+    def find_seq_quan_for_scientists(self):
+        for i in range(len(self.graph.unique_scientists_list)):
+            self.quan_of_sequences_for_sqientists.append(self.graph.graph_matrix[i][i])
+
+
+
+    def __next__(self):
+        return next(self.quan_of_sequences_for_sqientists), next(self.graph.unique_scientists_list)
+
+
+    def __iter__(self):
+        return self
+
+
